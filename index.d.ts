e4c45513c3831c2e2c68f536d2a8a87a51334def
François Risoud <francois.risoud@gmail.com>
2024-12-09 15:59:44 +0100

    fix: declarations as module

--- index.d.ts
+++ index.d.ts
@@ -1,43 +1,41 @@
-declare module "vue3-popper" {
-  import { DefineComponent } from "vue";
+import { DefineComponent } from "vue";
 
-  interface Props {
-    placement?:
-      | "auto"
-      | "auto-start"
-      | "auto-end"
-      | "top"
-      | "top-start"
-      | "top-end"
-      | "bottom"
-      | "bottom-start"
-      | "bottom-end"
-      | "right"
-      | "right-start"
-      | "right-end"
-      | "left"
-      | "left-start"
-      | "left-end";
-    disableClickAway?: boolean;
-    offsetSkid?: string;
-    offsetDistance?: string;
-    hover?: boolean;
-    show?: boolean;
-    disabled?: boolean;
-    openDelay?: number | string;
-    closeDelay?: number | string;
-    zIndex?: number | string;
-    arrow?: boolean;
-    arrowPadding?: string;
-    interactive?: boolean;
-    locked?: boolean;
-    content?: string;
-    strategy?: "absolute" | "fixed";
-    "onOpen:popper"?: () => void;
-    "onClose:popper"?: () => void;
-  }
-
-  const Popper: DefineComponent<Props>;
-
-  export default Popper;
+declare interface Props {
+  placement?:
+    | "auto"
+    | "auto-start"
+    | "auto-end"
+    | "top"
+    | "top-start"
+    | "top-end"
+    | "bottom"
+    | "bottom-start"
+    | "bottom-end"
+    | "right"
+    | "right-start"
+    | "right-end"
+    | "left"
+    | "left-start"
+    | "left-end";
+  disableClickAway?: boolean;
+  offsetSkid?: string;
+  offsetDistance?: string;
+  hover?: boolean;
+  show?: boolean;
+  disabled?: boolean;
+  openDelay?: number | string;
+  closeDelay?: number | string;
+  zIndex?: number | string;
+  arrow?: boolean;
+  arrowPadding?: string;
+  interactive?: boolean;
+  locked?: boolean;
+  content?: string;
+  strategy?: "absolute" | "fixed";
+  "onOpen:popper"?: () => void;
+  "onClose:popper"?: () => void;
 }
+
+declare const Popper: DefineComponent<Props>;
+
+export default Popper;
